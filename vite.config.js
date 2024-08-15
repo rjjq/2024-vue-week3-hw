b9b20c58ff984dfdf27e6c20d53f73f7dcf2a519
rjjq <rrrjjjqqq@gmail.com>
2024-08-15 18:23:27 +0800

    update README

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/2024-vue-week3-hw/',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
